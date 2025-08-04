a317454cbf1934113c9ace27098b134b66d88c3b
PropertyPete1 <peter@lifestyledesignrealty.com>
2025-08-04 18:22:38 -0500

    fix: update Settings model to match frontend UI fields exactly - add dropboxToken, runwayApiKey, fix field names

--- src/models/settings.js
+++ src/models/settings.js
@@ -4,17 +4,23 @@ const settingsSchema = new mongoose.Schema({
   instagramToken: String,
   igBusinessId: String,
   facebookPageId: String,
+
   youtubeToken: String,
   youtubeRefreshToken: String,
   youtubeChannelId: String,
   youtubeClientId: String,
   youtubeClientSecret: String,
-  openaiKey: String,
-  s3AccessKeyId: String,
+
+  dropboxToken: String,
+  mongoURI: String,
+
+  runwayApiKey: String,
+  openaiApiKey: String,
+
+  s3AccessKey: String,
   s3SecretAccessKey: String,
   s3BucketName: String,
   s3Region: String,
-  mongoUri: String
 }, { timestamps: true });
 
 module.exports = mongoose.model('SettingsClean', settingsSchema);
